7b98d7b8fecca6227b708b20fb3b3359e575e6a8
yingjie <yingjie_qiao@outlook.com>
2022-05-03 01:08:02 +0800

    round robin load balancing + load testing

--- Client/Client.go
+++ Client/Client.go
@@ -4,8 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/rpc"
 	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
 type PushEvent struct {
@@ -21,6 +25,26 @@ type ClientGetResp struct {
 	Values []string
 }
 
+func roundRobin() []string {
+	var nodeList []string
+	envNodeList := os.Getenv("NODE_LIST")
+
+	if envNodeList == "" {
+		log.Println("Using default node list")
+		nodeList := []string{":8081", ":8082", ":8083", ":8084", ":8085"}
+		return nodeList
+	} else {
+		arr := strings.FieldsFunc(envNodeList, func(r rune) bool {
+			return r == ','
+		})
+		for i, node := range arr {
+			nodeList[i] = node
+		}
+	}
+
+	return nodeList
+}
+
 func main() {
 	for {
 		fmt.Printf("Enter 1: Scalability Test\n" +
@@ -31,7 +55,7 @@ func main() {
 		char, _, _ := reader.ReadRune()
 		if char == '1' {
 			fmt.Println("Scalability Test")
-			// TODO
+			scalabilityTest()
 			break
 		} else if char == '2' {
 			fmt.Println("Vector Clock/Correctness Test")
@@ -45,8 +69,69 @@ func main() {
 	}
 }
 
+func scalabilityTest() {
+	var wg sync.WaitGroup
+	n := 350 // 2*n write + read
+	writeFails := 0
+	readFails := 0
+	start := time.Now()
+
+	for i := 1; i <= n; i++ {
+		nodeList := roundRobin()
+		rand.Seed(time.Now().UnixNano())
+		node := nodeList[rand.Intn(len(nodeList))]
+
+		client, err := rpc.DialHTTP("tcp", node)
+		if err != nil {
+			log.Fatal("Dialing:", err)
+		}
+		log.Printf("Successfulling connected to node %s", node)
+		args := PushEvent{"Bruce", "Banner"}
+		wg.Add(1)
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			writeReply := ClientPushResp{}
+			err = client.Call("Server.PushValue", &args, &writeReply)
+			if err != nil {
+				writeFails++
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+
+			readReply := ClientGetResp{}
+			err = client.Call("Server.GetValue", "Bruce", &readReply)
+			if err != nil {
+				readFails++
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	end := time.Now()
+	interval := end.Sub(start).Seconds()
+	log.Printf("%d write fails for %d operations, %d write fails for for %d operations. \n",
+		writeFails, n, readFails, n)
+	log.Printf("Throughout: %f RPS", float64(n*2)/float64(interval))
+}
+
 func defaultTest() {
-	client, err := rpc.DialHTTP("tcp", ":8081") // connect to the node
+	//nodeList := roundRobin()
+	//rand.Seed(time.Now().Unix())
+	//node := nodeList[rand.Intn(len(nodeList))]
+	//
+	//client, err := rpc.DialHTTP("tcp", node)
+	//if err != nil {
+	//	log.Fatal("Dialing:", err)
+	//}
+	//log.Printf("Successfulling connected to node %s", node)
+
+	client, err := rpc.DialHTTP("tcp", ":8081")
 	if err != nil {
 		log.Fatal("Dialing:", err)
 	}
