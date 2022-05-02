b5cdab9ced3f67d663f01c60ac96a60049863738
yingjie <yingjie_qiao@outlook.com>
2022-05-02 18:12:45 +0800

    organize test cases

--- Client/Client.go
+++ Client/Client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/rpc"
+	"os"
 )
 
 type PushEvent struct {
@@ -20,26 +22,48 @@ type ClientGetResp struct {
 }
 
 func main() {
+	for {
+		fmt.Printf("Enter 1: Scalability Test\n" +
+			"Enter 2: Vector Clock/Correctness Test\n" +
+			"Enter any other key to run Default KV Test\n")
+
+		reader := bufio.NewReader(os.Stdin)
+		char, _, _ := reader.ReadRune()
+		if char == '1' {
+			fmt.Println("Scalability Test")
+			// TODO
+			break
+		} else if char == '2' {
+			fmt.Println("Vector Clock/Correctness Test")
+			// TODO
+			break
+		} else {
+			fmt.Println("Default KV Test")
+			defaultTest()
+			break
+		}
+	}
+}
+
+func defaultTest() {
 	client, err := rpc.DialHTTP("tcp", ":8081") // connect to the node
 	if err != nil {
 		log.Fatal("Dialing:", err)
 	}
 
-	// reply := ClientPushResp{}
+	reply := ClientPushResp{}
 	reply2 := ClientGetResp{}
 
-	// args := PushEvent{"Bruce", "Banner"}
-
-	// err = client.Call("Server.PushValue", &args, &reply)
-	// if err != nil {
-	// 	log.Fatal("RPC error:", err)
-	// }
-	// fmt.Printf("Push value response: %v\n", reply) // should be true, pushed successfully
+	args := PushEvent{"Bruce", "Banner"}
+	err = client.Call("Server.PushValue", &args, &reply)
+	if err != nil {
+		log.Fatal("RPC error:", err)
+	}
+	fmt.Printf("Push value response: %v\n", reply)
 
 	err = client.Call("Server.GetValue", "Bruce", &reply2)
 	if err != nil {
 		log.Fatal("RPC error:", err)
 	}
-	fmt.Printf("Get value response: Bruce %v\n", reply2) // should be "There" as value was pushed successfully
-
+	fmt.Printf("Get value response: Bruce %v\n", reply2)
 }
